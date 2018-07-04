898ae325c24853cba2d485e83e00912b0b81c978
Erishen <lei_sun@Ctrip.com>
2018-07-04 11:28:27 +0800

    add bin/rrs.js

---
+++ bin/rrs.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/**
+ * Created by lei_sun on 2018/7/4.
+ */
+
+console.log('rrs start');
+
+const spawn = require('cross-spawn');
+const args = process.argv.slice(2);
+
+console.log('rrs_args', args);
+
