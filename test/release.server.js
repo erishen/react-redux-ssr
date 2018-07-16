11ae699d50f7a133390dad68e81d3aec0096efe3
Erishen <leisun8309@gmail.com>
2018-07-16 22:58:43 +0800

    update

--- test/release.server.js
+++ test/release.server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Index = require('../index');
 
-console.log(__dirname, path.resolve(__dirname, '../config'));
-const Config = require(path.resolve(__dirname, '../config')).default;
+console.log(__dirname, path.resolve(__dirname, '../../../config'));
+const Config = require(path.resolve(__dirname, '../../../config')).default;
 
 Index.release(Config, 'index');
