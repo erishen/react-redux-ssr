4ae96494093bc06fb3b4c43a005a0d671ee92da7
Erishen <lei_sun@Ctrip.com>
2018-07-24 10:16:35 +0800

    update

--- test/release.server.js
+++ test/release.server.js
@@ -4,4 +4,5 @@ const Index = require('../index');
 console.log(__dirname, path.resolve(__dirname, '../../../config'));
 const Config = require(path.resolve(__dirname, '../../../config')).default;
 
+console.log('Config', Config);
 Index.release(Config, 'index');
