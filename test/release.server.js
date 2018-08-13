4f0ae0cd7a1ea36512f9cc8b7d49f89a36d8b214
Erishen <leisun8309@gmail.com>
2018-08-13 21:10:10 +0800

    update

--- test/release.server.js
+++ test/release.server.js
@@ -3,7 +3,7 @@ const Index = require('../index');
 
 let Config = null;
 
-if(0){ // 作为第三方 node_modules 发布时使用
+if(1){ // 作为第三方 node_modules 发布时使用
     Config = require(path.resolve(__dirname, '../../../config')).default;
 }
 else { // 作为本地测试时使用
