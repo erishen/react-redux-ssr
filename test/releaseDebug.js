29d9517c561430b7717a437b75eeab024ea4b635
Erishen <lei_sun@Ctrip.com>
2018-08-14 15:22:43 +0800

    update

--- test/releaseDebug.js
+++ test/releaseDebug.js
@@ -1,5 +1,8 @@
+/**
+ * Created by lei_sun on 2018/8/14.
+ */
 require('babel-register');
 require("babel-polyfill");
 process.env.isModule = 1;
-process.env.isDebug = 0;
+process.env.isDebug = 1;
 require('./release.server');
